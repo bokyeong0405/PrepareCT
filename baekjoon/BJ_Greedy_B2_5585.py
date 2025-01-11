dbb1c4bf4b3bdb3cead7d274c37be0a617aa762d
bokyeong0405 <bokyeong0405@khu.ac.kr>
2025-01-12 00:28:52 +0900

    greedy algo commit

---
+++ baekjoon/BJ_Greedy_B2_5585.py
@@ -0,0 +1,14 @@
+price = int(input())
+coin = [500, 100, 50, 10, 5, 1]
+cnt = 0
+idx = 0
+change = 1000 - price
+
+while(change):
+    if change >= coin[idx]:
+        change = change - coin[idx]
+        cnt += 1
+    else:
+        idx += 1
+
+print(cnt)
---
+++ baekjoon/BJ_Greedy_S4_2839.py
@@ -0,0 +1,19 @@
+total = int(input())
+
+if total % 5 == 0:
+    print(total // 5)
+else:
+    cnt = 0
+    while(total > 0):
+        total -= 3
+        cnt += 1
+        if total % 5 == 0:
+            print(cnt + total//5)
+            break
+        elif total == 1 or total == 2:
+            print(-1)
+            break
+        elif total == 0:
+            print(cnt)
+
+
