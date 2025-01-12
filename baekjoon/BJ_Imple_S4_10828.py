bf2bf77f184b2623f2984b4596e11d2c7041676a
bokyeong0405 <bokyeong0405@khu.ac.kr>
2025-01-12 22:11:55 +0900

    greedy algo commit

---
+++ baekjoon/BJ_Imple_S4_10828.py
@@ -0,0 +1,27 @@
+import sys
+
+num_req = int(sys.stdin.readline())
+
+s = []
+
+for i in range(num_req):
+    req = sys.stdin.readline().split()
+    if req[0] == 'push':
+        s.append(req[1])
+    elif req[0] == 'pop':
+        if len(s) > 0:
+            print(s.pop())
+        else:
+            print(-1)
+    elif req[0] == 'size':
+        print(len(s))
+    elif req[0] == 'empty':
+        if len(s) > 0:
+            print(0)
+        else:
+            print(1)
+    elif req[0] == 'top':
+        if len(s) > 0:
+            print(s[-1])
+        else:
+            print(-1)
